374c546079f407b5bab4733002dd94fd1704d7b4
Herman-Gathege <siinamtechstudio@gmail.com>
2024-09-14 14:37:00 +0300

    added now

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,47 +1,56 @@
 from datetime import datetime
+from typing import List, Dict, Union
+
+class Enrollment:
+    all = []  # Class variable to store all Enrollment instances
+
+    def __init__(self, student, course, enrollment_date):
+        self.student = student
+        self.course = course
+        self.enrollment_date = enrollment_date
+        Enrollment.all.append(self)  # Track all enrollments
+
+    def get_enrollment_date(self):
+        return self.enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
+
 class Student:
     def __init__(self, name):
         self.name = name
-        self._enrollments = []
-
-    def enroll(self, course):
-        if isinstance(course, Course):
-            enrollment = Enrollment(self, course)
-            self._enrollments.append(enrollment)
-            course.add_enrollment(enrollment)
-        else:
-            raise TypeError("course must be an instance of Course")
-
-    def get_enrollments(self):
-        return self._enrollments.copy()
-
-class Course:
-    def __init__(self, title):
-
-        self.title = title
-        self._enrollments = []
+        self._enrollments = []  # List of Enrollment objects
+        self._grades = {}  # Dictionary of {Enrollment: grade}
 
     def add_enrollment(self, enrollment):
-        if isinstance(enrollment, Enrollment):
-            self._enrollments.append(enrollment)
-        else:
+        if not isinstance(enrollment, Enrollment):
             raise TypeError("enrollment must be an instance of Enrollment")
+        self._enrollments.append(enrollment)
+        self._grades[enrollment] = None  # Initialize with no grade
 
-    def get_enrollments(self):
-        return self._enrollments.copy()
+    def course_count(self):
+        return len(self._enrollments)
 
+    def aggregate_average_grade(self):
+        total_grades = sum(grade for grade in self._grades.values() if grade is not None)
+        num_courses = len(self._grades)
+        if num_courses == 0:
+            return 0
+        average_grade = total_grades / num_courses
+        return average_grade
 
-class Enrollment:
-    all = []
-    
-    def __init__(self, student, course):
-        if isinstance(student, Student) and isinstance(course, Course):
-            self.student = student
-            self.course = course
-            self._enrollment_date = datetime.now()
-            type(self).all.append(self)
-        else:
-            raise TypeError("Invalid types for student and/or course")
+    def set_grade(self, enrollment, grade):
+        if not isinstance(enrollment, Enrollment):
+            raise TypeError("enrollment must be an instance of Enrollment")
+        if enrollment not in self._grades:
+            raise ValueError("Enrollment not found in student's records")
+        self._grades[enrollment] = grade
 
-    def get_enrollment_date(self):
-        return self._enrollment_date
+class Course:
+    def __init__(self, name):
+        self.name = name
